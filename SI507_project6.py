0673580ad5848aeb6fd2cc2fa8d07f26c0e90325
conary <stefande@umich.edu>
2017-11-22 12:22:30 -0500

    Added insert function

--- SI507_project6.py
+++ SI507_project6.py
@@ -6,7 +6,9 @@ import requests
 from config import *
 import sys
 import json
+from csv import DictReader
 
+db_connection, db_cursor = None, None
 
 # Write code / functions to set up database connection and cursor here.
 def get_connection_and_cursor():
@@ -28,30 +30,88 @@ def setup_database():
     # Invovles DDL commands
     # DDL --> Data Definition Language
     # CREATE, DROP, ALTER, RENAME, TRUNCATE
-    cur.execute("DROP TABLE IF EXISTS Sites")
-    cur.execute("DROP TABLE IF EXISTS States")
-    conn, cur = get_connection_and_cursor()
     
-    cur.execute("""CREATE TABLE IF NOT EXISTS States(
-        ID INTEGER,
+    conn, db_cursor = get_connection_and_cursor()
+    db_cursor.execute("DROP TABLE IF EXISTS Sites")
+    db_cursor.execute("DROP TABLE IF EXISTS States")
+    
+    db_cursor.execute("""CREATE TABLE IF NOT EXISTS States(
+        ID SERIAL PRIMARY KEY,
         name VARCHAR(40) UNIQUE
         )""")
 
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS Sites(
+    db_cursor.execute("""CREATE TABLE IF NOT EXISTS Sites(
         ID SERIAL PRIMARY KEY,
+        Name VARCHAR(128) UNIQUE,
         Type VARCHAR(128) NOT NULL,
         State_ID INTEGER REFERENCES States(ID),
-        location VARCHAR(255),
-        Description (TEXT)
+        Location VARCHAR(255),
+        Description TEXT
         )""")
 
 
+    # db_cursor.execute(INSERT INTO  States(Name) VALUES(%S)) RETURNING ID
+    # result = db_cursor.fetchone()
+    # print(result)
     
     conn.commit()
 
 
+
+def insert_into_db(conn, cur, table, data_dict, no_return=False):
+    """Accepts connection and cursor, table name, dictionary that represents one row, and inserts data into table. (Not the only way to do this!)"""
+    column_names = data_dict.keys()
+    print(column_names, "column_names") # for debug
+    if not no_return:
+        query = sql.SQL('INSERT INTO {0}({1}) VALUES({2}) ON CONFLICT DO NOTHING RETURNING id').format(
+            sql.SQL(table),
+            sql.SQL(', ').join(map(sql.Identifier, column_names)),
+            sql.SQL(', ').join(map(sql.Placeholder, column_names))
+        )
+    else:
+        query = sql.SQL('INSERT INTO {0}({1}) VALUES({2}) ON CONFLICT DO NOTHING').format(
+            sql.SQL(table),
+            sql.SQL(', ').join(map(sql.Identifier, column_names)),
+            sql.SQL(', ').join(map(sql.Placeholder, column_names))
+        )
+    query_string = query.as_string(conn) # thanks to sql module
+    cur.execute(query_string, data_dict) # will mean that id is in cursor, because insert statement returns id in this function
+    if not no_return:
+        return cur.fetchone()['id']
+
+
+
+
+
+
+
+get_connection_and_cursor();
 setup_database();
+# state_dict={'name':'Arkansas'}
+# insert(db_connection, db_cursor, "States", state_dict)
+state_dict={'name':'Michigan'}
+state_id = insert_into_db(db_connection, db_cursor, "States", state_dict)
+the_reader = DictReader(open('michigan.csv', 'r'))
+for line_dict in the_reader:
+  print(line_dict)
+  
+  insert(db_connection, db_cursor, "Sites", line_dict)
+
+
+
+
+
+# state_dict={'name':'California'}
+# insert(db_connection, db_cursor, "States", state_dict)
+
+
+
+
+
+
+db_connection.commit()
+
 
 # Write code / functions to create tables with the columns you want and all database setup here.
 
